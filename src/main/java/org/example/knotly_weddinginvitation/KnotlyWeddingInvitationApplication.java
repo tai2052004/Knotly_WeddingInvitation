2f57cdf926f5545f6bb0ea09f99d6bc07b87a807
tai2052004 <tai2052004@gmail.com>
2025-09-13 22:39:49 +0700

    init project

---
+++ src/main/java/org/example/knotly_weddinginvitation/KnotlyWeddingInvitationApplication.java
@@ -0,0 +1,13 @@
+package org.example.knotly_weddinginvitation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnotlyWeddingInvitationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KnotlyWeddingInvitationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/knotly_weddinginvitation/KnotlyWeddingInvitationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.knotly_weddinginvitation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnotlyWeddingInvitationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
