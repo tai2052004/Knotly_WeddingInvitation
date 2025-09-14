3bbb0c4a986aaff2f7350808b34b099b5de9dc9a
tai2052004 <tai2052004@gmail.com>
2025-09-14 22:06:29 +0700

    Add landing page

---
+++ src/main/java/com/KnotlyWeddingInvitationApplication.java
@@ -0,0 +1,29 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.awt.Desktop;
+import java.net.URI;
+
+@SpringBootApplication
+public class KnotlyWeddingInvitationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KnotlyWeddingInvitationApplication.class, args);
+        openBrowser("http://localhost:8080");
+    }
+
+    private static void openBrowser(String url) {
+        try {
+            Thread.sleep(2000); // Đợi 5 giây để chắc chắn server đã chạy
+            if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
+                Desktop.getDesktop().browse(new URI(url));
+            } else {
+                System.out.println( url);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
